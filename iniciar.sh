3c43efb34ea6cd674b24540791a8f749898402c3
leonardo <leonardofreed@gmail.com>
2020-12-19 14:16:15 -0300

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -31,7 +31,7 @@ apt-get install redis -y > /dev/null 2>&1
 apt-get install screen -y > /dev/null 2>&1
 apt-get install zip -y > /dev/null 2>&1
 
-wget https://www.dropbox.com/s/j9bpk6m27egkwkp/gerarusuario-sshplus.sh?dl=0 -O gerarusuario.sh; chmod +x gerarusuario.sh > /dev/null
+wget https://raw.githubusercontent.com/leonardofreed/bot-ssh3g/master/gerarusuario-sshplus.sh?dl=0 -O gerarusuario.sh; chmod +x gerarusuario.sh > /dev/null
 
 wget https://github.com/leonardofreed/bot-ssh3g/raw/master/%40admysshbot.zip -O bot.zip && unzip bot.zip > /dev/null
 
